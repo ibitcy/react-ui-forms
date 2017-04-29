17c124a2ac287e9474d5be8f1f0ac258a386b1d6
Sergey Egorov <sergey.e@expertoption.com>
2017-04-29 16:50:02 +0300

    index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,31 @@
-/// <reference path="./lib/components/ui/form/form.d.ts" />
-/// <reference path="./lib/components/ui/form/card-expiry-input.d.ts" />
-/// <reference path="./lib/components/ui/form/color-selector.d.ts" />
-/// <reference path="./lib/components/ui/form/country-selector.d.ts" />
-/// <reference path="./lib/components/ui/form/form-message.d.ts" />
-/// <reference path="./lib/components/ui/form/payment-card-input.d.ts" />
-/// <reference path="./lib/components/ui/form/phone-input.d.ts" />
-/// <reference path="./lib/components/ui/form/selector.d.ts" />
-/// <reference path="./lib/components/ui/form/text-area.d.ts" />
-/// <reference path="./lib/components/ui/form/text-input.d.ts" />
-/// <reference path="./lib/checkbox.d.ts" />
+import {ReactValidForm, ValidationType} from './lib/components/ui/form/form';
+import {UICardExpiryInput} from './lib/components/ui/form/card-expiry-input';
+import {UIColorSelector, UIFormColorItem} from './lib/components/ui/form/color-selector';
+import {UICountrySelector} from './lib/components/ui/form/country-selector';
+import {UIFormMessage, UIFormMessageType} from './lib/components/ui/form/form-message';
+import {UIPaymentCardInput} from './lib/components/ui/form/payment-card-input';
+import {UIPhoneInput} from './lib/components/ui/form/phone-input';
+import {UIFormSelector, UIFormSelectorItem} from './lib/components/ui/form/selector';
+import {UITextArea} from './lib/components/ui/form/text-area';
+import {UITextInput} from './lib/components/ui/form/text-input';
+import {UiFormCheckbox} from './lib/checkbox';
+
+export {
+	ReactValidForm,
+	ValidationType,
+	UICardExpiryInput,
+	UIColorSelector,
+	UIFormColorItem,
+	UICountrySelector,
+	UIFormMessage,
+	UIFormMessageType,
+	UIPaymentCardInput,
+	UIPhoneInput,
+	UIFormSelector,
+	UIFormSelectorItem,
+	UITextArea,
+	UITextInput,
+	UiFormCheckbox,
+}
+
+
